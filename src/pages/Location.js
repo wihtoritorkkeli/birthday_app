a9ca925f9ec9b01e2f3dc9c9469805c73afbc8ec
wihtoritorkkeli <awito001@edu.xamk.fi>
2022-07-08 21:00:49 +0300

    tt

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -2,9 +2,12 @@ import React from "react";
 
 function Location(){
     return(
-        <div className="locationBanner">
-            <h1>Usefull locations</h1>
-            <p className="valueProp">Check usefull locaiton infromation from this page</p>
+        <div className="margin-zero">
+            <div className="locationBanner">
+                <h1>Usefull locations</h1>
+                <p className="valueProp">Check usefull locaiton infromation from this page</p>
+            </div>
+            
         </div>
     );
 }
