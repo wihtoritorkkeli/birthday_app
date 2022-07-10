26c373dd0d49d61587606b7378961ded7d6388ac
wihtoritorkkeli <awito001@edu.xamk.fi>
2022-07-10 13:15:52 +0300

    säätöä

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,13 +1,10 @@
 import React from "react";
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 
 function Location(){
     return(
         <div className="margin-zero">
-            <div className="locationBanner">
-                <h1>Usefull locations</h1>
-                <p className="valueProp">Check usefull locaiton infromation from this page</p>
-            </div>
-            
+            <h1 className="locationH1">Usefull location infromation</h1>
         </div>
     );
 }
