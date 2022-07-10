34ccee9d018bcb3b35f894e8622378e725f726ac
wihtoritorkkeli <awito001@edu.xamk.fi>
2022-07-10 12:54:12 +0300

    re-did front page style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import './location.css';
 import React, { useState } from 'react';
 import Home from './pages/Home';
 import Location from './pages/Location';
 import Sitsit from './pages/Sitsit';
 import {Route, Link, Routes} from 'react-router-dom';
-import NavBar from './NavBar';
-import Footer from './Footer';
 import Lappi from './pages/Lappi';
 
 
@@ -14,14 +11,12 @@ function App() {
 
   return (
     <div className="App">
-      <NavBar />
       <Routes>
         <Route exact path="/" element={<Home />}/>
         <Route exact path="/location" element={<Location />}/>
         <Route exact path="/sitsit" element={<Sitsit />}/>
         <Route exact path='/lappi' element={<Lappi />} />
       </Routes>
-      <Footer/>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,35 +3,11 @@ import {Link} from 'react-router-dom';
 
 function Home(){
     return(
-        <div className="margin-zero">
-            <div className="heroBanner">
-                <h1>Welcome to <br/>W &amp; S birthday party app!</h1>
-                <p className="valueProp">Find all the information needed to have a great time</p>
-            </div>
-            <div className="basicInfo">
-                <h2>Warm welcome to the party!</h2>
-                <p>Hope you packed enough energy and liquids with you, because tonight is going to be a long night</p>
-                <p>Here's a quick review of tonights intinirary:</p>
-                <ul className="alignLeft">
-                    <li>Fashion Show and introductions</li>
-                    <li>Birthday Sitsit &#40;Including Oscar Awards&#41;</li>
-                    <li>Other fun activities</li>
-                </ul>
-                <br/>
-                <h3>Check usefull links from below:</h3>
-                <br/>
-            </div>
-            <div className="container">
-                <Link to="/location" className="link"><div className="linkBox" id="location">
-                    <h2>Location information</h2>
-                </div></Link>
-                <Link to="/sitsit" className="link"><div className="linkBox" id="sitsit">
-                    <h2>Sitsit</h2>
-                </div></Link>
-                <Link to="/lappi" className="link"><div className="linkBox" id="lappi">
-                    <h2>Lapin Energiaa</h2>
-                </div></Link>
-            </div>
+        <div className="front-page">
+            <h1>W &amp; S <br/>Birthday Bash</h1>
+            <Link to="/location"><button className="fp-btn">Location Information</button></Link><br/>
+            <Link to="sitsit"><button className="fp-btn">Sitsi Stuff</button></Link><br/>
+            <Link to="lappi"><button className="fp-btn">Lapin Energiaa</button></Link>
         </div>
     );
 }
