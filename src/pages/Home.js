74821b2b113c7ee7876fb33433aa7e5e1b4ef008
wihtoritorkkeli <awito001@edu.xamk.fi>
2022-07-10 16:46:47 +0300

    Back to previous

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 function Home(){
     return(
         <div className="front-page">
-            <h1>W &amp; S <br/>Birthday Bash!</h1>
+            <h1>W &amp; S <br/>Birthday Bash</h1>
             <Link to="/location"><button className="fp-btn">Location Information</button></Link><br/>
             <Link to="sitsit"><button className="fp-btn">Sitsi Stuff</button></Link><br/>
             <Link to="lappi"><button className="fp-btn">Lapin Energiaa</button></Link>
