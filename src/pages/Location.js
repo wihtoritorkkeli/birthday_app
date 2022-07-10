d0183f50d426b6f1c356075b7c1db44c8b65a36a
wihtoritorkkeli <awito001@edu.xamk.fi>
2022-07-10 17:16:42 +0300

    Location

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -8,6 +8,10 @@ function Location(){
         <div className="location-page">
             <div className="homeLink"><Link to="/"><FontAwesomeIcon icon={faHouseChimney} color={'white'} size="2x"/></Link></div>
             <h1 className="locationH1">Usefull location infromation</h1>
+            <div className="locationBox">
+                <h2>Töölönlahden tapahtumapuisto</h2>
+                <p>Lisätietoja tähän</p>
+            </div>
         </div>
     );
 }
